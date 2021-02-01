dc026906fc119c7f3f445866dd8b2b7a7935a402
Sharon Gamarragit config --global user.name Sharon <sharon2172542@correo.uis.edu.co>
2021-02-01 06:58:32 -0500

    Añadi parametros y la simulacion

--- Actividad.py
+++ Actividad.py
@@ -3,3 +3,18 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import flopy
+
+#parametros del modelo
+name = "tutorial01_mf6"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
+
+#Creando simulación de flopy
+sim = flopy.mf6.MFSimulation(
+    sim_name=name, exe_name=C:/12-DIPLOMADO/Programas/mf6.2.0/bin/mf6, version="mf6", sim_ws="."
+)
