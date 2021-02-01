cad44a67efb5993cbefe4aac81083d614267c171
Sharon Gamarragit config --global user.name Sharon <sharon2172542@correo.uis.edu.co>
2021-02-01 07:29:21 -0500

    Resultados

--- Actividad.py
+++ Actividad.py
@@ -94,5 +94,28 @@ success, buff = sim.run_simulation()
 if not success:
     raise Exception("MODFLOW 6 did not terminate normally.")
 
+#RESULTADOS-CAPA 1
+hds = flopy.utils.binaryfile.HeadFile(headfile)
+h = hds.get_data(kstpkper=(0, 0))
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[0], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%2.1f")
+
+#RESULTADO-MAPA DE LA CAPA 10
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[-1], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
+#RESULTADO-SECCIÓN TRANSVERSAL
+z = np.linspace(-H / Nlay / 2, -H + H / Nlay / 2, Nlay)
+fig = plt.figure(figsize=(5, 2.5))
+ax = fig.add_subplot(1, 1, 1, aspect="auto")
+c = ax.contour(x, z, h[:, 50, :], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
 
 
